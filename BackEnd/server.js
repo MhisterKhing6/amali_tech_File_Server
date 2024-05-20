586f0995221e4cd3e2f9f923e56f806ed657cb2b
------ <nlbcaw9875@localhost.localdomain>
2024-05-20 11:51:19 +0100

    chore: Backend folder Structure finalize

---
+++ BackEnd/server.js
@@ -0,0 +1,18 @@
+import Express from "express";
+import Cors from "cors"
+import configuration from "config"
+
+//server initializing
+const fileServer = Express()
+
+//middlewares
+fileServer.use(Cors()) //cross origin communication
+fileServer.use(Express.json()) // json body parsing
+
+fileServer.get("/", (req, res) => {
+    return res.send("ok i am working")
+})
+
+fileServer.listen(configuration.host.port,configuration.host.ip, () => {
+    console.log(`app is listening at http://${configuration.host.ip}:${configuration.host.port}`)
+})
