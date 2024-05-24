446440e9e99b75d4ade0e9fda86bc20e21335abd
------ <nlbcaw9875@localhost.localdomain>
2024-05-24 15:39:19 +0100

    chore: modified file model schema

--- BackEnd/models/file.js
+++ BackEnd/models/file.js
@@ -1,10 +1,10 @@
 import {Model, Schema } from "mongoose";
 const FileSchema = new Schema({
     title:     {type: String, required:true, unique},
+    description:   {type: String, required:true},
     downloads: {type: Number, default: 0},
     emailSent: {type:Number, default: 0},
     filePath:  {type:String, required:true},
-    fileName:  {type:String, required:true},
     updatedAt: {type:Date, default:Date.now},
     uploadedBy:{type:String, required:true}//fk to users
 
