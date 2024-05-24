7a325dbd85a1d60c7eda49706f4e319e0c84d302
------ <nlbcaw9875@localhost.localdomain>
2024-05-25 00:38:26 +0100

    chore:added interface for admin dashboard

---
+++ FrontEnd/src/pages/adminDasboardPage.js
@@ -0,0 +1,28 @@
+/** protected route */
+import { getToken } from "../utils/localstorage.js"
+import { token } from "../utils/config.js"
+import { Navigate} from "react-router-dom"
+import { UserProfile } from "../components/userProfile.js"
+import { AdminNavBar } from "../components/navBaradmin.js"
+import { AdminDashboard } from "../components/dashboard.js"
+const AdminDashboardPage = () => {
+    const authenticated = getToken(token.adminTokenKey)
+    
+    /*if(!authenticated) {
+        return <Navigate to="/admin/login" />
+    } else { */
+      return  (
+      <>
+      <div style={{minHeight: "25vh"}} className="my-0">
+      <AdminNavBar />
+        <UserProfile />
+      </div>
+      <div style={{minHeight: "65vh"}} className="my-0">
+      <AdminDashboard />
+      </div>
+      </>
+      )
+    
+}
+
+export {AdminDashboardPage}
