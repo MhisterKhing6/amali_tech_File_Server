a0d4aa9da4bde782bc0ce9dd78813e3eb3f82f98
------ <nlbcaw9875@localhost.localdomain>
2024-05-26 12:18:49 +0100

    chore: donwload front end

--- FrontEnd/src/components/listFile.js
+++ FrontEnd/src/components/listFile.js
@@ -1,9 +1,16 @@
 import {Button} from 'react-bootstrap';
 import {Card, DropdownButton, ButtonGroup, Dropdown} from 'react-bootstrap';
 import { LiaDownloadSolid } from "react-icons/lia";
+import { getFromBackend } from '../utils/backendCalls';
+import { getToken } from '../utils/localstorage';
+import { token } from '../utils/config';
 
-function FileItem() {
-const download = (id) => {}
+function FileItem({fileId}) {
+const downloadFile =async (fileId) => {
+  let authToken = getToken(token.authToken) ? getToken(token.customerTokenKey) : getToken(token.adminTokenKey)
+  let downloadFile =  await getFromBackend(`/files/download/${fileId}`, authToken)
+  alert("downloading")
+}
   return (
     <Card>
       <Card.Header as="h5">Featured</Card.Header>
@@ -12,8 +19,8 @@ const download = (id) => {}
           With supporting text below as a natural lead-in to additional content.
         </Card.Text>
         <DropdownButton as={ButtonGroup} title={<LiaDownloadSolid style={{width:"80px", height:"30px"}}/>} id="bg-nested-dropdown">
-        <Dropdown.Item eventKey="1" onClick={(val) => {
-            
+        <Dropdown.Item eventKey="1" onClick={ async (val) => {
+            await downloadFile(fileId)
         }}>Download</Dropdown.Item>
         <Dropdown.Item eventKey="2">Email</Dropdown.Item>
       </DropdownButton>
