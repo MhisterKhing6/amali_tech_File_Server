a0da951f6f0a0e28ff13613caa7d27fcccfb6d28
------ <nlbcaw9875@localhost.localdomain>
2024-05-28 05:14:38 +0100

    chore: added route to save admin

--- BackEnd/controllers/adminController.js
+++ BackEnd/controllers/adminController.js
@@ -5,6 +5,42 @@ import { getFileNameFromTitle, saveUpolaodFileDisk } from "../utils/FileHandler.
 
 class AdminController {
     /** */
+    static registerAdmin = async (req, res) => {
+        /**
+         * register: registers admin, testing purposes
+         * @param {object} req: request object
+         * @param {object} res: response
+         * @returns {object} : json response of user detials
+         */
+        let userDetails = req.body
+        //check if all required user details are given
+        if(!(userDetails.email && userDetails.password && userDetails.name))
+            return res.status(400).json({"message": "fields missing"})
+        //check if the user is already register
+        let alreadyUser = await UserModel.findOne({email: userDetails.email})
+        if(alreadyUser)
+            return res.status(400).json({"message": "user with the same email already registered"})
+        
+        //save the user to the database
+        try {
+            let passwordHash = sha1(userDetails.password)
+            let userDb = UserModel({role:"admin", emailVerified:true, name:userDetails.name, email:userDetails.email, passwordHash})
+            //send verificaion message
+            let verificationCode = generateSecretNumber()
+            //save information in the Verify token database
+            await userDb.save()
+            //verify object
+            let verifyObject = {"userId":userDb._id.toString(), verificationCode, type:"email"}
+            let verifyToken = await new VerifTokenModel(verifyObject).save()
+            //asynchroneously send verificatio message
+            sendEmailVerification(userDb, verificationCode)
+            res.status(201).json({"id": userDb._id, verificationId: verifyToken._id.toString()})
+        } catch(err) {
+            console.log(err)
+            return res.status(501).json({"message": "internal error"})
+        }
+    }
+
     static uploadFile = async (req, res) => {
         /**
          * uploadFile: upload file handler for admin users
